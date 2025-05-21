8b09c7506b4c0bb028b3cb46746cb622ca0742b8
buvanapavitra <buvanapavitra16@gmail.com>
2025-05-21 20:16:59 +0300

    second commit on server.js

--- family-tree-backend/server.js
+++ family-tree-backend/server.js
@@ -38,7 +38,7 @@ app.get(/(.*)/, (req, res) => {
 //const PORT = process.env.PORT || 5000;
 //app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   connectDB();
   console.log("Server started at http://localhost:" + PORT);
 });
